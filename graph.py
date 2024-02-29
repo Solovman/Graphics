a94b94bf0534768186219eddbdf8980245f3076b
Илья Соловьев <solovevilya3101@gmail.com>
2024-02-29 14:57:49 +0200

    Minor changes in the comment

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 from matplotlib.pyplot import *
 import numpy as np
 
-# Function graph sin(x/2) * e^x
+# Function graph sin(x / 2) * e^x
 x = np.arange(-2 * np.pi, 2 * np.pi, 0.01)
 y = np.sin(x / 2) * np.e ** x
 plot(x, y)
